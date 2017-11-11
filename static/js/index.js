40488c6563c36fdbd690eda579790e9527ceb557
ayushmaang <ayushmaang@gmail.com>
2017-11-11 23:18:06 +0000

    Refactor front end code

--- static/js/index.js
+++ static/js/index.js
@@ -1,65 +1,33 @@
-$(function(){
-    var signedout = false
-    window.onload = function(){
-        var config = {
-        apiKey: "AIzaSyCZI7-GNlZg4V3g2HquIu2hVIn1s9YciYw",
-        authDomain: "campusmarketplace-d8706.firebaseapp.com",
-        databaseURL: "https://campusmarketplace-d8706.firebaseio.com",
-        projectId: "campusmarketplace-d8706",
-        storageBucket: "campusmarketplace-d8706.appspot.com",
-        messagingSenderId: "39930550082"
-        };
-        firebase.initializeApp(config);
+$(function() {
+
+    var dimmerButton = $('.dim');
+    var dimmer = $('.dimmer');
+    var exit = $('.exit');
+    dimmerButton.on('click', function() {
+        dimmer.show();
+    });
+    exit.on('click', function() {
+        dimmer.hide();
+    });
     
-        // window.location = "login.html"
+    $('#login').click(function(e){
         
-        firebase.auth().onAuthStateChanged(firebaseUser => {
-                if (firebaseUser){
-                    console.log(firebaseUser)
-                    console.log(document.getElementById("sign").innerHTML)
-                    document.getElementById("sign").innerHTML = "Your Account" //"Welcome  " + firebaseUser.displayName
-                }
-                else{
-                    document.getElementById("sign").innerHTML = "Log In"
-                    console.log("No one logged in")
-                }
-        })
-        
-        $('#sign').click(function(e) {
-            console.log("clicked")
-            // document.getElementById("sign").innerHTML = "Sign Out"
-            // window.location = "login.html"
-            firebase.auth().onAuthStateChanged(firebaseUser => {
-                    if (firebaseUser){
-                        console.log(firebaseUser)
-                        window.location = "account.html"
-                        // $(this).attr("href","index.html");
-                        // document.getElementById("sign").innerHTML = "Welcome  " + firebaseUser.displayName
-                        // const auth = firebase.auth();
-                        // const promise = firebase.auth().signOut();
-                        // signedout = true
-                        // promise.catch(e => console.log(e.message, "Couldn't log out"))
-                        // console.log("User: ", firebase.auth().currentUser)
-                        // console.log("Signed Out")
-                    }
-                    else{
-                        if(!signedout){
-                            console.log('not logged in')
-                            window.location = "login.html"
-                            document.getElementById("sign").innerHTML = "Log In"
-                        }
-                    }
-                })
-                
-            if(document.getElementById("sign").innerHTML === "Log In") window.location = "login.html"
-            
-            return false
-        });
-    };
-    
-    
+    });
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
 });
-
-
-
-

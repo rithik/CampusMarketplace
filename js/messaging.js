341b3caecf74b9ca08af6e37591e9fe0c7f1f887
ayushmaang <ayushmaang@gmail.com>
2017-11-12 05:12:56 +0000

    fix front end

--- js/messaging.js
+++ js/messaging.js
@@ -1,11 +1,37 @@
-var app = require('express')();
+var express = require("express")
+var app = express();
 var http = require('http').Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+var path = require('path');
+
+app.get("/", function(req, res){
+    var testHtmlPath = path.resolve(__dirname,'..','templates','messages.html');
+    res.sendFile(testHtmlPath);
+})
+
+app.listen(process.env.PORT,process.env.IP, function(){
+})
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
-    
+
+io.emit('some event', { for: 'everyone' });
+
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
